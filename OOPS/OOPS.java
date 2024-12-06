2758cdc74dcf4d8df819119062c90d3656aed449
prapti-gupta-1805 <prapti18056@gmail.com>
2024-12-06 14:41:14 +0530

    ?

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -4,12 +4,6 @@ Class: group of those entities
 
 Classes have attributes (properties) and functions (behaviors)
 
-Example:
-Object: Pen
-- Color: String (blue)
-- Tip: int (5)
-- Methods: setColor(), setTip()
-
 Access Modifiers:
 - Private, Default, Protected, Public
 
@@ -18,94 +12,98 @@ Getters & Setters:
 - Set: to modify the value
 
 'this': keyword used to refer to the current object.
-*/
-
-public class OOPS {
+*/public class OOPS {
     public static void main(String[] args) {
         // Pen Example
         Pen p1 = new Pen(); // Created a pen object called p1
-        p1.setColor("Blue");
-        System.out.println("Pen Color: " + p1.getColor());
-        p1.setTip(5);
-        System.out.println("Pen Tip: " + p1.getTip());
+        p1.Color = "Blue"; 
+        System.out.println("Pen Color: " + p1.Color); // Using the getter to get color
+        p1.Tip = 5;
+        System.out.println("Pen Tip: " + p1.Tip); // Using getter to get the tip size
 
         p1.setColor("Yellow"); // Modifying color using setter
         System.out.println("Updated Pen Color: " + p1.getColor());
 
         // BankAccount Example
-        BankAccount myAcc = new BankAccount();
-        myAcc.setUsername("PraptiGupta");
-        myAcc.setPassword("abcd"); // Password set securely
+        BankAccount myAcc = new BankAccount(); // Created a BankAccount object called myAcc
+        myAcc.setUsername("PraptiGupta"); // Set the username
+        //myAcc.password = "abcd"; - won't work because password is private. have to use a setter to access it
+        myAcc.setPassword("abcd"); // Set the password
         System.out.println("Bank Username: " + myAcc.getUsername());
-
-        // Pencil Example
-        Pencil p2 = new Pencil();
-        p2.setColor("Green");
-        System.out.println("Pencil Color: " + p2.getColor());
-        p2.setTip(8);
-        System.out.println("Pencil Tip: " + p2.getTip());
-    }
-}
-
-class BankAccount {
-    private String username;
-    private String password;
-
-    // Getters and Setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+        System.out.println("Bank Password: " + myAcc.getPassword()); // can be done because getPassword is defined inside the class Bankaccount
     }
 }
 
 class Pen {
-    private String color;
-    private int tip;
+    String Color; // Private attribute, can't be accessed directly
+    int Tip; // Private attribute, can't be accessed directly
 
-    // Getters
+    // Getter for color
     public String getColor() {
-        return color;
+        return Color;
     }
 
+    // Getter for tip size
     public int getTip() {
-        return tip;
+        return Tip;
     }
 
-    // Setters
+    // Setter for color
     public void setColor(String newColor) {
-        this.color = newColor;
+        this.Color = newColor;
     }
 
+    // Setter for tip size
     public void setTip(int newTip) {
-        this.tip = newTip;
+        this.Tip = newTip;
+    }
+}
+
+class BankAccount {
+    String username; // Private attribute for username
+    private String password; // Private attribute for password
+
+    // Getter for username
+    public String getUsername() {
+        return username;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter for username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Setter for password (password can be set but not retrieved directly)
+    public void setPassword(String password) {
+        this.password = password; // Private to protect security
     }
 }
 
 class Pencil {
-    private String color;
-    private int tip;
+    private String color; // Private attribute for color
+    private int tip; // Private attribute for tip size
 
-    // Getters
+    // Getter for color
     public String getColor() {
         return color;
     }
 
+    // Getter for tip size
     public int getTip() {
         return tip;
     }
 
-    // Setters
+    // Setter for color
     public void setColor(String newColor) {
         this.color = newColor;
     }
 
+    // Setter for tip size
     public void setTip(int newTip) {
         this.tip = newTip;
     }

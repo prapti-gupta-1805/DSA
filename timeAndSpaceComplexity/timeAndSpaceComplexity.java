68047a5628b3a85c43cb13c30f89d1f42feea5e2
prapti-gupta-1805 <prapti18056@gmail.com>
2024-12-24 00:22:15 +0530

    ?

--- timeAndSpaceComplexity/timeAndSpaceComplexity.java
+++ timeAndSpaceComplexity/timeAndSpaceComplexity.java
@@ -5,9 +5,5 @@ Time complexity measures the amount of time an algorithm takes to run as a funct
 eg. O(1), O(log n), O(n), O(n log n), O(n²), O(2ⁿ), O(n!)
 Space Complexity:
 Space complexity measures the amount of memory space an algorithm uses as a function of the input size.
-
-- Optimized Bubble Sort -
-  best case - O(n)
-  worst case - O(n²)
 */
 }

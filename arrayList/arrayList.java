a12b1ddde71a0eda13275b8c4fb58e86613794ec
prapti-gupta-1805 <prapti18056@gmail.com>
2024-12-29 22:57:36 +0530

    ?

---
+++ arrayList/arrayList.java
@@ -0,0 +1,10 @@
+package arrayList;
+import java.util.*;
+
+public class arrayList {
+    public static void main(String[] args) {
+        //creation
+        ArrayList<Integer> list = new ArrayList<>();
+
+    }
+}

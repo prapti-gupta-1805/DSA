b6bd939bd6d11137a7cf17867a2116e889f8a408
Prapti <prapti18056@gmail.com>
2025-01-09 14:37:25 +0530

    ?

--- java/10_Backtracking/Backtracking.java
+++ java/10_Backtracking/Backtracking.java
@@ -2,4 +2,31 @@ package 10_Backtracking;
 
 public class Backtracking {
 
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void changeArr(int arr[], int i, int val) {
+        //base case
+        if (i == arr.length) {
+            printArr(arr);
+            return;
+        }
+        //recursion
+        arr[i] = val;
+        changeAree(arr, i+1,val+1);
+        arr[i] -= 2;
+    }
+
+
+    public static void main(String args[]) {
+        int arr[]= new int[5];
+
+        //simple depiction of backtracking
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
 }

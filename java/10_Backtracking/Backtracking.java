6fe6aa8bb1fcaeb0c9d0a61345a32c332392d594
prapti-gupta-1805 <prapti18056@gmail.com>
2025-01-11 13:30:41 +0530

    Update Backtracking.java

--- java/10_Backtracking/Backtracking.java
+++ java/10_Backtracking/Backtracking.java
@@ -1,7 +1,6 @@
 package 10_Backtracking;
 
 public class Backtracking {
-
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -17,16 +16,35 @@ public class Backtracking {
         }
         //recursion
         arr[i] = val;
-        changeArr(arr, i+1,val+1);
-        arr[i] -= 2;
+        changeArr(arr, i+1,val+1); //fn call
+        arr[i] -= 2; //backtracking step
     }
 
+    public static void findSubsets(String str, String s, int i) {
+        //base case
+        if(i == str.length()) {
+            if (s.length() == 0) {
+                System.out.println(".");
+            } else {
+                System.out.println(s);
+            }
+        return;
+        }
+        //recursion
+        //yes choice
+        findSubsets(str,s+str.charAt(i),i+1);
+        //no choice
+        findSubsets(str,s,i+1);
+    }
 
+    
     public static void main(String args[]) {
         int arr[]= new int[5];
 
         //simple depiction of backtracking
         changeArr(arr, 0, 1);
         printArr(arr);
+        String str = "abc";
+        findSubsets(str,"",0);
     }
 }

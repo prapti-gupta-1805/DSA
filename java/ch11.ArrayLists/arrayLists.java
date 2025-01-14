ead02f972076eca813fc7b51456b666b36adaed4
Prapti <prapti18056@gmail.com>
2025-01-14 23:57:08 +0530

    ArrayList

--- java/ch11.ArrayLists/arrayLists.java
+++ java/ch11.ArrayLists/arrayLists.java
@@ -30,5 +30,48 @@ public class arrayLists {
         //contains
         System.out.println(list.contains(2)); //boolean
         System.out.println(list.contains(11));
+
+        //size
+        System.out.println(list.size());
+
+        //print arraylist in reverse
+        for(int i = list.size()-1; i>=0; i-- ) {
+            System.out.println(list.get(i));
+        }
+
+        //sorting
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list, Collections.reverseOrder()); //comparator
+        System.out.println(list);
+
+        //2D ArrayLists
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+        mainlist.add(list1);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(4);
+        list2.add(6);
+        list2.add(8);
+        list2.add(10);
+        mainlist.add(list2);
+
+        //printing 2D ArrayList
+        for(int i = 0; i <mainlist.size(); i++) {
+            ArrayList<Integer> curr = mainlist.get(i);
+            for(int j = 0; j <curr.size();j++) {
+                System.out.print(curr.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 }

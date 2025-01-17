2801a5243c2dc7fcd1352311696ab4461d475dbf
Prapti <prapti18056@gmail.com>
2025-01-17 23:33:44 +0530

    ArrayList

--- java/ch11.ArrayLists/PairSum.java
+++ java/ch11.ArrayLists/PairSum.java
@@ -36,7 +36,7 @@ public class PairSum {
 
     public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<>();
-        list.add(1); list.add(2); list.add(3); list.add(4); list.add(5); list.add(6);
+    list.add(1); list.add(2); list.add(3); list.add(4); list.add(5); list.add(6);
     int target = 5;
 
     boolean result1 = PairSum1(list, target);

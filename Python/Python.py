05d8fafa8276559c2526c3ffeb596d9a38b76ae7
Prapti <prapti18056@gmail.com>
2025-01-29 13:44:07 +0530

    deletion and updation

--- Python/Python.py
+++ Python/Python.py
@@ -1,1 +1,12 @@
+print("hello world!")
 
+# single line comment
+'''
+multiple
+line
+comment
+'''
+
+a = input("What is your name?  ")
+print("Hello " + a + "!!")
+print("Hello" ,a, "!!") #adds space itself

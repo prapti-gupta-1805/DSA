882f194de62d64b2e5f411797833d44abf20e9d4
Prapti <prapti18056@gmail.com>
2025-01-29 15:31:18 +0530

    python basics

--- Python/Python.py
+++ Python/Python.py
@@ -1,5 +1,6 @@
 print("hello world!")
 
+
 # single line comment
 '''
 multiple
@@ -7,6 +8,30 @@ line
 comment
 '''
 
+print("a", end = "") #puts nothing at EOL instead of newline
+print("a") #end = '\n' by 
 a = input("What is your name?  ")
 print("Hello " + a + "!!")
-print("Hello" ,a, "!!") #adds space itself
+print("Hello" ,a, "!!", sep = "") #puts no space between two values
+print("Hello" ,a, "!!") #adds space by default
+print(f"Hello {a}")
+
+
+#to print quotes
+#print("Hello "friend"") - won't work
+print('Hello "friend"')
+print("hello \"friend\"")
+
+
+#.strip() function removes whitespaces from string
+name1 = name2 = name3 = "                 PRAPTI                 "
+name4 = "prapti"
+
+name1 = name1.lstrip() #for left spaces
+print("/" + name1 + "/", sep="")
+name2 = name2.rstrip() #for right spaces
+print("/" + name2 + "/", sep="")
+name3 = name3.strip() #for all spaces
+print("/" + name3 + "/", sep="")
+name4 = name4.strip('i') #to remove argument passed
+print(name4)

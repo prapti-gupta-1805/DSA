deea416e65971adba1cf75ef25f8750329021387
Prapti <prapti18056@gmail.com>
2025-02-04 12:34:38 +0530

    more python

--- Python/sayings.py
+++ Python/sayings.py
@@ -1,5 +1,13 @@
+def main():
+    hello("prapti")
+    goodbye("prapti")
+
+
 def hello(name):
     print(f"hello, {name}")
 
 def goodbye(name):
     print(f"goodbye, {name}")
+
+if __name__ == "__main__":
+    main()

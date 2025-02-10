5b17f2f3e437fa85d16785db711b539197031969
Prapti <prapti18056@gmail.com>
2025-02-10 22:59:57 +0530

    python

--- Python/basics/OOPS.py
+++ Python/basics/OOPS.py
@@ -1,16 +1,21 @@
 class Student:
-        def __init__(self, name, house):
+        def __init__(self, name, branch):
             self.name = name
-            self.house = house
+            self.branch = branch
 
 def main():
     student = get_student()
     print(f"{student.name} from {student.branch}")
 
 def get_student():
-    student = Student()
-    student.name = input("name: ")
-    student.branch = input("branch:  ")
+    # student = Student()
+    # student.name = input("name: ")
+    # student.branch = input("branch:  ")
+    # return student
+
+    name = input("name: ")
+    branch = input("branch:  ")
+    student = Student(name,branch) #constructor call
     return student
 
 if __name__ == "__main__" :

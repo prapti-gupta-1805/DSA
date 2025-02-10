77500e85b0df72ce90a36417583cc70a67010b4a
Prapti <prapti18056@gmail.com>
2025-02-10 22:59:39 +0530

    python

--- Python/basics/OOPS.py
+++ Python/basics/OOPS.py
@@ -1,9 +1,11 @@
 class Student:
-        ...
+        def __init__(self, name, house):
+            self.name = name
+            self.house = house
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(f"{student.name} from {student.branch}")
 
 def get_student():
     student = Student()

38e835b488da6e75c33dd130ce8fc7a41c8a274c
Prapti <prapti18056@gmail.com>
2025-04-08 23:43:59 +0530

    ll

--- java/ch12.LinkedLists/LinkedList.java
+++ java/ch12.LinkedLists/LinkedList.java
@@ -1,150 +1,54 @@
-
-
-public class LinkedList {
-    class Node {
+public class LinkedList{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    Node head;
-    Node tail;
-    int size;
+    public static Node head;
+    public static Node tail;
 
-    public LinkedList() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    public void addFirst(int data) { // O(1)
+    //methods
+    public void addFirst(int data) {
+        //create new node
         Node newNode = new Node(data);
-        size++;
 
+        //if ll is empty
         if (head == null) {
             head = tail = newNode;
             return;
         }
 
+        //make new node point to head
         newNode.next = head;
+        //label new node as head
         head = newNode;
     }
 
-    public void addLast(int data) { // O(1)
+    public void addLast(int data) {
+        //create new node
         Node newNode = new Node(data);
-        size++;
 
+        //check if ll is empty
         if (head == null) {
             head = tail = newNode;
             return;
         }
-
+        //make tail.next point to new node
         tail.next = newNode;
+        //label new node as tail
         tail = newNode;
     }
 
-    public void add(int index, int data) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Invalid index");
-        }
-
-        if (index == 0) {
-            addFirst(data);
-            return;
-        }
-
-        if (index == size) {
-            addLast(data);
-            return;
-        }
-
-        Node newNode = new Node(data);
-        Node temp = head;
-        int i = 0;
-
-        while (i < index - 1) {
-            temp = temp.next;
-            i++;
-        }
-
-        newNode.next = temp.next;
-        temp.next = newNode;
-        size++;
-    }
-
-    public int removeFirst() {
-        if (head == null) {
-            throw new IllegalStateException("Cannot remove from an empty list");
-        }
-
-        int val = head.data;
-        head = head.next;
-        size--;
-
-        if (head == null) { // If list becomes empty, reset tail
-            tail = null;
-        }
-
-        return val;
-    }
-
-    public int removeLast() {
-        if(size == 0) {
-            System.out.println("LL is empty");
-            return Integer.MIN_VALUE;
-        } else if(size == 1) {
-            int val = head.data;
-            head = tail = null;
-            return val;
-        }
-        Node prev = head;
-        for(int i = 0; i<size-2; i++) {
-            prev = prev.next;
-        }
-        int val = prev.next.data;
-        prev.next = null;
-        tail = prev;
-        size--;
-
-        return val;
-    }
-
-    public void printList() {
-        if (head == null) {
-            System.out.println("LL is empty");
-            return;
-        }
-
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         LinkedList ll = new LinkedList();
+        ll.head = new Node(1);
+        ll.head.next = new Node(2);
 
-        ll.addFirst(2);
         ll.addFirst(1);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.add(2, 3);
-
-        ll.printList();
-        System.out.println("Size: " + ll.size);
-
-        System.out.println("Removed: " + ll.removeFirst());
-        ll.printList();
-        System.out.println("Size: " + ll.size);
-
-        System.out.println("Removed: " + ll.removeLast());
-        ll.printList();
-        System.out.println("Size: " + ll.size);
     }
 }

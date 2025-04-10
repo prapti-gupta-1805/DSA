59e2beca67ae1ab9936eb436d83a4cb9d12b1ca0
Prapti <prapti18056@gmail.com>
2025-04-10 15:08:50 +0530

    ll

--- java/ch12.LinkedLists/LinkedList.java
+++ java/ch12.LinkedLists/LinkedList.java
@@ -11,12 +11,13 @@ public class LinkedList{
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //methods
     public void addFirst(int data) {
         //create new node
         Node newNode = new Node(data);
-
+        size++;
         //if ll is empty
         if (head == null) {
             head = tail = newNode;
@@ -32,7 +33,7 @@ public class LinkedList{
     public void addLast(int data) {
         //create new node
         Node newNode = new Node(data);
-
+        size++;
         //check if ll is empty
         if (head == null) {
             head = tail = newNode;
@@ -44,11 +45,72 @@ public class LinkedList{
         tail = newNode;
     }
 
+    public void addMiddle(int idx, int data) {
+
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node temp = head;
+        int count = 0;
+
+        while (count < idx - 1 && temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        
+        if (temp == null) {
+            addLast(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        size++; 
+
+        newNode.next = temp.next;
+
+        temp.next = newNode;
+    }
+
+    public void removeFirst() {
+        if(size == 0) {
+            System.out.println("ll is empty");
+            return;
+        }
+        else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            System.out.println(val);
+            size--;
+            return;
+        }
+
+        int val = head.data;
+        head = head.next;
+        System.out.println(val);
+        size--;
+    }
+
+    public void removeLast() {
+        
+    }
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
-        ll.head = new Node(1);
-        ll.head.next = new Node(2);
 
         ll.addFirst(1);
+        ll.addLast(2);
+
+        ll.printList();
+
+        System.out.println(ll.size);
     }
 }
